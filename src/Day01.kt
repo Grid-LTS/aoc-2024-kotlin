019bcd0c17d2bfa52cdc8d7aef8d3ca89efb1dd1
Gerd Friemel <gerd.friemel@friemel-tech.de>
2024-12-01 18:17:24 +0100

    feat: setup project and solved Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,54 @@
+import java.util.Collections.min
+import kotlin.math.abs
+
 fun main() {
+    fun getLists(input: List<String>) : Pair<MutableList<Int>, MutableList<Int>> {
+        val listLeft: MutableList<Int> = mutableListOf()
+        val listRight: MutableList<Int> = mutableListOf()
+        for (i in input.indices) {
+            if (input[i].isBlank()){
+                continue
+            }
+            input[i].split(" ").map { it.trim() }.filter{ it.isNotBlank() }.also {
+                listLeft.add(it[0].toInt())
+                listRight.add(it[1].toInt())
+            }
+        }
+        return Pair(listLeft, listRight)
+    }
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (listLeft, listRight) = getLists(input)
+        var sum = 0
+        val rounds = listLeft.size
+        for (k in 0..<rounds) {
+            val l = min(listLeft)
+            val r = min(listRight)
+            sum += abs(r - l)
+            listLeft.removeAt(listLeft.indexOf(l))
+            listRight.removeAt(listRight.indexOf(r))
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        val (listLeft, listRight) = getLists(input)
+        for (l in listLeft.indices) {
+            val times = listLeft[l] * listRight.filter {it ==  listLeft[l] }.size
+            sum += times
+        }
+        return sum
     }
 
+
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    // check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
